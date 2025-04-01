8a883b0acb81e4aa0c8a44a4a20e659dd8ac1d0b
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-04-01 17:18:28 +0500

    logic to render gifs

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,8 +1,36 @@
 import { useState, useEffect } from "react";
 
 function GameBoard() {
-    const API_KEY = import.meta.env.VITE_GIPHY_API_KEY;
+    const [gifs, setGifs] = useState([])
 
-    const response = await fetch (
+    const API_KEY = import.meta.env.VITE_GIPHY_API_KEY;
+    
+    const fetchGifs = async (query) => {
+        const response = await fetch (
+            `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=loading&limit=10&rating=g&lang=en`
+        )
+        const data = await response.json()
+        setGifs(data.data);    
+    }
+
+    useEffect(() => {
+        fetchGifs();
+    }, []);
+
+
+    return (
+        <div>
+            {gifs.length === 0 ? (
+                <p>Loading...</p>
+            ) : (
+                <div>
+                    {gifs.map((gif) => (
+                        <img key={gif.id} src={gif.images.fixed_height.url} alt="GIF" />
+                    ))}
+                </div>
+            )}
+        </div>
     )
 }
+
+export default GameBoard;
