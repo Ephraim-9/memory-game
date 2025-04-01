8b75173d0cffd9dcb72fb84d649b0c5f0c49618d
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-04-01 23:35:56 +0500

    add search query for gifs

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,17 +1,22 @@
 import { useState, useEffect } from "react";
 
 function GameBoard() {
-    const [gifs, setGifs] = useState([])
-
+    const [gifs, setGifs] = useState([]);
+    const [query, setQuery] = useState("");
     const API_KEY = import.meta.env.VITE_GIPHY_API_KEY;
     
     const fetchGifs = async (query) => {
         const response = await fetch (
-            `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=loading&limit=10&rating=g&lang=en`
+            `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${encodeURIComponent(query)}&limit=10&rating=g&lang=en`
         )
         const data = await response.json()
         setGifs(data.data);    
     }
+
+    const handleSearchSubmit = (e) => {
+        e.preventDefault();  
+        fetchGifs(query);
+    };
 
     useEffect(() => {
         fetchGifs();
@@ -20,6 +25,19 @@ function GameBoard() {
 
     return (
         <div>
+            <div>
+            <h1>Search GIFs</h1>
+
+            <form onSubmit={handleSearchSubmit}>
+                <input 
+                    type="text" 
+                    placeholder="Search GIFs..."
+                    value={query} 
+                    onChange={(e) => setQuery(e.target.value)} 
+                />
+                <button type="submit">Search</button>
+            </form>
+        </div>
             {gifs.length === 0 ? (
                 <p>Loading...</p>
             ) : (
